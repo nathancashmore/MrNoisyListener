9f6cdcd23c29664535bb429bbdd989d3ae0fbed5
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-10-18 11:32:08 +0100

    Provide multiple random greetings

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ python /home/pi/snowboy/startup_lights.py &
 
 while :
   do
-	python /home/pi/snowboy/singleWord.py /home/pi/resources/Mr_Noisy.pmdl
-	aplay /home/pi/resources/Welcome.wav &
+    option=$(( $RANDOM % 2 ))
+    welcome="Welcome_${option}.wav"
+
+	python /home/pi/snowboy/singleWord.py /home/pi/resources/Mr_Noisy_2.pmdl
+	aplay /home/pi/resources/${welcome} &
 	python /home/pi/google/pushtotalk_led.py --once
   done
